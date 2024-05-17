d660c17acf2424fb730b2b595c473140a5beaacb
kamilia <k_djaid@esi.dz>
2024-05-17 11:24:05 +0100

    fixing the nav menu appearence problem: hidden or visible

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,9 @@
+/* main button: allows to show the nav menu and the about page */
 document.getElementById("home_btn").addEventListener("click", function () {
-  console.log("Kamilia");
+  document.getElementById("nav_menu").style.display = "block";
+});
+
+/* clicking on home link on the nav menu allows to hide the nav menu */
+document.getElementById("nav_link_home").addEventListener("click", function () {
+  document.getElementById("nav_menu").style.display = "none";
 });
