39b630fc994ecad6e90c8c2bd7ad50fad00d1d24
kamilia <k_djaid@esi.dz>
2024-05-25 17:57:44 +0100

    fixing bug hamburger button

--- docs/index.js
+++ docs/index.js
@@ -4,9 +4,11 @@ document.getElementById("home_btn").addEventListener("click", function () {
 });
 
 /* humburger button: allows to show the humburger menu */
-document.getElementById("humburger").addEventListener("click", function () {
-  document.getElementById("humburger-menu").style.display = "block";
-});
+document
+  .getElementById("humburger-wrapper")
+  .addEventListener("click", function () {
+    document.getElementById("humburger-menu").style.display = "block";
+  });
 
 /* clicking on home link on the nav menu allows to hide the nav menu */
 document.getElementById("nav_link_home").addEventListener("click", function () {
